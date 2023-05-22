b7f566400862c63df69bd59cbd1199bac45e2869
bwernard <bwernard@gmail.com>
2023-05-21 22:33:36 -0300

    pacote inicial de código

---
+++ logic.js
@@ -0,0 +1,53 @@
+let inventorio = ['Sword','Rope','Clothes','Food'];
+let aventureiro = [];
+
+let listaLoja = document.getElementById("InventorioLoja");
+let listaAventureiro = document.getElementById("InventorioMochila");
+
+escreverInventorio();
+
+function escreverInventorio(){
+    inventorio.forEach((item)=>{
+        let li = document.createElement("li");
+        li.innerText = item;
+        li.id = ("itemDaLoja_" + item);
+        listaLoja.appendChild(li);
+
+        let buttonLoja = document.createElement("button");    
+        buttonLoja.innerText = "+";
+        buttonLoja.id = ("buyButton_" + item);
+        buttonLoja.className = "BuyButton";
+        buttonLoja.addEventListener('click', function(){
+            aventureiro.splice(aventureiro.length, 0, item);
+            atualizaMochila();
+        });
+        li.appendChild(buttonLoja); 
+    })
+}
+
+function escreverListaAventureiro(){
+    let SellButtonCounter = 0;
+    aventureiro.forEach((item)=>{
+        let li = document.createElement("li");
+        li.innerText = item;
+        li.id = ("listaDoAventureiro_" + item);
+        listaAventureiro.appendChild(li);
+
+        let buttonMochila = document.createElement("button");
+        buttonMochila.innerText = "-";
+        buttonMochila.id = (SellButtonCounter);
+        buttonMochila.className = "SellButton";
+        buttonMochila.addEventListener('click', function(){
+            aventureiro.splice(buttonMochila.id,1);
+            atualizaMochila();
+            SellButtonCounter--;
+        })
+        li.appendChild(buttonMochila);
+        SellButtonCounter++;
+    })
+}
+
+function atualizaMochila () {
+    listaAventureiro.replaceChildren();
+    escreverListaAventureiro();
+}
